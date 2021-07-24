bac40b67506bd2246ea604769027b84a0b051573
Rikanthan <rikanthanricky@gmail.com>
2021-07-24 10:25:41 +0530

    actions recognised success

--- app/src/main/java/com/example/i_email/Actions.java
+++ app/src/main/java/com/example/i_email/Actions.java
@@ -4,18 +4,149 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class Actions extends AppCompatActivity {
-
+    TextToSpeech speechText;
+    TextView actionText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actions);
+        actionText = findViewById(R.id.seeActions);
+        speechText = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = speechText.setLanguage(Locale.ENGLISH);
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                        Log.e("TTS","Initialization is successfull");
+                    }
+                } else {
+                    Log.e("TTS", "Initialization failed");
+                }
+            }
+        });
+        final SpeechRecognizer mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        final Intent mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                Locale.getDefault());
+        mSpeechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                ArrayList<String> matches = results
+                        .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                actionText.setText(matches.get(0));
+                if(matches != null)
+                {
+                    if(matches.get(0).contains("inbox"))
+                    {
+                        Intent i1 = new Intent(Actions.this,ShowInbox.class);
+                        startActivity(i1);
+                    }
+                    else if(matches.get(0).contains("send"))
+                    {
+                        Intent i2 = new Intent(Actions.this,ShowSentbox.class);
+                        startActivity(i2);
+                    }
+                    else if(matches.get(0).contains("write")
+                            ||  matches.get(0).contains("right")
+                                    ||  matches.get(0).contains("contacts"))
+                    {
+                        Intent i3 = new Intent(Actions.this,Contacts.class);
+                        startActivity(i3);
+                    }
+                }
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+        });
+        findViewById(R.id.action_button).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_UP:
+                        mSpeechRecognizer.stopListening();
+                        //msg.setHint("You will see input here");
+                        speak(  "              Your action recognizes successfully");
+                        break;
+
+                    case MotionEvent.ACTION_DOWN:
+                        mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+//                        msg.setText("");
+//                        msg.setHint("Listening...");
+
+                        break;
+                }
+                return false;
+            }
+        });
     }
-    public void Action(View v)
-    {
-        Intent i = new Intent(this,Home.class);
-        startActivity(i);
+    private void speak(String text) {
+        // String text = "Welcome";//to voice-email application. please press mic button and speak few words to send a message.";
+        speechText.setPitch(0.8f);
+        speechText.setSpeechRate(0.7f);
+        speechText.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+    @Override
+    protected void onDestroy() {
+        if (speechText != null) {
+            speechText.stop();
+            speechText.shutdown();
+        }
+        super.onDestroy();
     }
 }
